b2bafb4f29d1847d72adf81a41949a52e4d88339
d4rkfella <georgi.panov@darkfellanetwork.com>
2026-04-05 15:06:15 +0000

    test

--- bazarr/main.tf
+++ bazarr/main.tf
@@ -23,15 +23,6 @@ provider "apko" {
   extra_packages = ["wolfi-baselayout"]
 }
 
-data "apko_config" "this" {
-  config_contents = file("${path.module}/apko.yaml")
-}
-
-data "apko_tags" "this" {
-  config         = data.apko_config.this.config
-  target_package = "bazarr"
-}
-
 module "apko" {
   source  = "chainguard-dev/apko/publisher"
   version = "0.0.18"
@@ -39,29 +30,15 @@ module "apko" {
   config = file("${path.module}/apko.yaml")
 
   target_repository = "ghcr.io/d4rkfella/bazarr"
+}
 
-  # optional but useful
-  extra_packages = ["wolfi-baselayout"]
+data "apko_tags" "this" {
+  config         = module.apko.config
+  target_package = "bazarr"
 }
 
 resource "oci_tag" "this" {
   for_each   = toset(data.apko_tags.this.tags)
   digest_ref = module.apko.image_ref
   tag        = each.value
-}
-
-
-output "image_ref" {
-  value       = module.apko.image_ref
-  description = "The fully-qualified index digest."
-}
-
-output "arch_digests" {
-  value       = module.apko.arch_to_image
-  description = "Map of architecture to fully-qualified digest ref."
-}
-
-output "image_tags" {
-  value       = data.apko_tags.this.tags
-  description = "Tags derived from the bazarr package version."
 }
