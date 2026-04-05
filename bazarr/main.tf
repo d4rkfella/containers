a484f45ec72b1bedc37df90666912d11e45346b9
d4rkfella <georgi.panov@darkfellanetwork.com>
2026-04-05 13:17:14 +0000

    test

--- bazarr/main.tf
+++ bazarr/main.tf
@@ -1,6 +1,7 @@
 terraform {
   required_providers {
     apko = { source = "chainguard-dev/apko" }
+    cosign = { source = "chainguard-dev/cosign" }
   }
   backend "s3" {
     key = "bazarr/terraform.tfstate"
@@ -30,6 +31,31 @@ resource "apko_build" "this" {
   repo    = "ghcr.io/d4rkfella/bazarr"
   config  = data.apko_config.this.config
   configs = data.apko_config.this.configs
+}
+
+resource "apko_publish" "this" {
+  config_contents = data.apko_config.this.config_contents
+  repo            = "ghcr.io/d4rkfella/bazarr"
+  tags            = data.apko_tags.this.tags 
+}
+
+resource "cosign_sign" "this" {
+  image = apko_publish.this.index_type == "" ? apko_publish.this.image_ref : apko_publish.this.index_ref
+}
+
+resource "cosign_attest" "this" {
+  image = cosign_sign.this.signed_ref
+
+  dynamic "predicates" {
+    for_each = apko_build.this.sboms
+    content {
+      type = "https://spdx.dev/Document"
+      file = {
+        path   = predicates.value
+        sha256 = filebase64sha256(predicates.value)
+      }
+    }
+  }
 }
 
 output "sboms" {
