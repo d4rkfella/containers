d0fa5f96f0ed70fb828586f608c145aa2e67a6fd
d4rkfella <georgi.panov@darkfellanetwork.com>
2026-04-05 16:16:40 +0000

    test

--- modules/apko-image/main.tf
+++ modules/apko-image/main.tf
@@ -2,6 +2,19 @@ variable "image_name" {}
 variable "config_file" {}
 variable "target_repository" {}
 
+terraform {
+  required_providers {
+    apko = {
+      source  = "chainguard-dev/apko"
+      version = ">= 0.29.10"
+    }
+    cosign = {
+      source  = "chainguard-dev/cosign"
+      version = ">= 0.2.7"
+    }
+  }
+}
+
 module "apko" {
   source            = "chainguard-dev/apko/publisher"
   version           = "0.0.18"
