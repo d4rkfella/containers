6fff8393dd0e649d8e1166cd82a83dc987ffd4ea
d4rkfella <georgi.panov@darkfellanetwork.com>
2026-05-15 05:28:54 +0000

    add new brrpolice image

---
+++ brrpolice/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    apko = { source = "chainguard-dev/apko" }
+    oci = { source = "chainguard-dev/oci"}
+  }
+  backend "s3" {
+    key = "brrpolice/terraform.tfstate"
+  }
+}
+
+provider "apko" {
+  default_archs = ["amd64", "arm64"]
+
+  extra_keyring = [
+    "https://packages.darkfellanetwork.com/artifactory/wolfi-os/melange.rsa.pub"
+  ]
+
+  build_repositories = [
+    "https://packages.darkfellanetwork.com/artifactory/wolfi-os/latest/main"
+  ]
+
+  extra_packages = ["wolfi-baselayout"]
+}
+
+module "apko" {
+  source  = "chainguard-dev/apko/publisher"
+  config = file("${path.module}/apko.yaml")
+  target_repository = "ghcr.io/d4rkfella/${basename(abspath(path.module))}"
+}
+
+data "apko_tags" "this" {
+  config         = module.apko.config
+  target_package = "brrpolice"
+}
+
+resource "oci_tag" "this" {
+  for_each   = toset(data.apko_tags.this.tags)
+  digest_ref = module.apko.image_ref
+  tag        = each.value
+}
