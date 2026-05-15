4f425b1a4f42081e5a4bb83889b2e406b4604673
d4rkfella <georgi.panov@darkfellanetwork.com>
2026-05-15 19:01:02 +0300

    test

--- vaultwarden/main.tf
+++ vaultwarden/main.tf
@@ -3,9 +3,7 @@ terraform {
     apko = { source = "chainguard-dev/apko" }
     oci = { source = "chainguard-dev/oci"}
   }
-  backend "s3" {
-    key = "${basename(abspath(path.module))}/terraform.tfstate"
-  }
+  backend "s3" {}
 }
 
 provider "apko" {
